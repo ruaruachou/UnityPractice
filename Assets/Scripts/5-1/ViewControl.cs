8f398464bb19641d4e84b5319cefe35d1fd7cc3f
John Doe <johndoe@example.com>
2023-05-02 02:33:44 +0800

    5-2凌晨

--- Assets/Scripts/5-1/ViewControl.cs
+++ Assets/Scripts/5-1/ViewControl.cs
@@ -3,20 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ViewControl : MonoBehaviour
+
 {
-    public float x;
-    public float y;
-    private Transform cameraTransform;
+    public float rotateSpeed = 1;
     // Start is called before the first frame update
     void Start()
     {
-        cameraTransform.transform = new Transform();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        x = Input.GetAxis("Mouse X");
-        y = Input.GetAxis("Mouse Y");
+
+
+        float x = Input.GetAxis("Mouse X");
+        float y = Input.GetAxis("Mouse Y");
+        if(x!=0||y!=0)
+        Rotate(x,y);
+    }
+
+    private void Rotate(float x,float y)
+    {
+        x *= rotateSpeed;
+        y *= rotateSpeed;
+
+        this.transform.Rotate(-y * Time.deltaTime, 0, 0);
+        this.transform.Rotate(0, x * Time.deltaTime, 0, Space.World);
     }
 }
