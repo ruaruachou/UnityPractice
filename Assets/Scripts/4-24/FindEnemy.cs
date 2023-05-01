dae1043f916e1f18049edb431f5fdfe60863a9f0
John Doe <johndoe@example.com>
2023-05-01 10:39:58 +0800

    5-1

--- Assets/Scripts/4-24/FindEnemy.cs
+++ Assets/Scripts/4-24/FindEnemy.cs
@@ -17,11 +17,27 @@ public class FindEnemy : MonoBehaviour
         }
         return minHPenemy;
     }
+    //方法：查找距离本对象距离最近的Enemy并返回
+    public Enemy FindEnemyByMinDistance(Enemy[] allEnemies)
+    {
+        Enemy minDisEnemy= allEnemies[0];
+        float minDistance=Vector3.Distance(this.transform.position,minDisEnemy.transform.position);
+        for (int i = 0; i < allEnemies.Length; i++)
+        {
+            if (minDistance > Vector3.Distance(this.transform.position, allEnemies[i].transform.position))
+            {
+                minDistance= Vector3.Distance(this.transform.position, allEnemies[i].transform.position);
+                minDisEnemy = allEnemies[i];
+            }
+        }
+        return minDisEnemy;
+    }
     private void Start()
     {
-        Enemy[]allEnemies=FindObjectsOfType<Enemy>();
+        Enemy[] allEnemies = FindObjectsOfType<Enemy>();
         Enemy min = FindEnemyByMinHp(allEnemies);
+        Enemy minDisEnemy = FindEnemyByMinDistance(allEnemies);
         min.GetComponent<MeshRenderer>().material.color = Color.red;
+        minDisEnemy.GetComponent<MeshRenderer>().material.color = Color.blue;
     }
-
 }
